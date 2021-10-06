1f2c17e7bf0a4cb6f04252c3eeb4825860e38552
Ravinder Deol <57337436+ravinderdeol@users.noreply.github.com>
2021-10-06 18:32:56 +0200

    read from rinkeby

---
+++ scripts/read_grade.py
@@ -0,0 +1,12 @@
+from brownie import GradeStorage, accounts, config
+
+# read from a deployed contract on the rinkeby test network
+# get the last item in the array
+# brownie is aware of the abi and address
+def read_contract():
+    grade_storage = GradeStorage[-1]
+    print(grade_storage.readGrade())
+
+
+def main():
+    read_contract
